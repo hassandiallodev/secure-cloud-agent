3fb76ff99d35790cff10c97d9960807025b49687
hassan <hassan.diallo@ac-versailles.fr>
2026-02-05 21:47:11 +0100

    Ajout du monorepo Maven et d’un test JUnit pour sec-agent

---
+++ sec-agent/src/test/java/io/github/hassandiallodev/SecAgentTest.java
@@ -0,0 +1,13 @@
+package io.github.hassandiallodev;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SecAgentTest {
+
+    @Test
+    public void shouldPass() {
+        assertTrue(true);
+    }
+}
